74941fdeb409151cd26c7af3297ca5ebf5c8383b
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-16 16:32:42 -0500

    🫴 write the picture entity  structure

--- backend/src/picture/entities/picture.entity.ts
+++ backend/src/picture/entities/picture.entity.ts
@@ -1,1 +1,17 @@
-export class Picture {}
+import { Product } from 'src/product/entities/product.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Picture {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  imagePath: string;
+
+  @Column('json')
+  coordinates: { x1: number; y1: number; x2: number; y2: number };
+
+  @ManyToOne(() => Product, (product) => product.pictures)
+  product: Product;
+}
