31ccffd33a403deca0285d6bc549df8601e6a917
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-16 18:16:43 -0500

    💸 fix the default controller testing

--- backend/src/picture/picture.controller.spec.ts
+++ backend/src/picture/picture.controller.spec.ts
@@ -1,17 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PictureController } from './picture.controller';
 import { PictureService } from './picture.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Picture } from './entities/picture.entity';
+import { Repository } from 'typeorm';
 
 describe('PictureController', () => {
   let controller: PictureController;
+  let service: PictureService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PictureController],
-      providers: [PictureService],
+      providers: [
+        PictureService,
+        { provide: getRepositoryToken(Picture), useClass: Repository },
+      ],
     }).compile();
 
     controller = module.get<PictureController>(PictureController);
+    service = module.get<PictureService>(PictureService);
   });
 
   it('should be defined', () => {
