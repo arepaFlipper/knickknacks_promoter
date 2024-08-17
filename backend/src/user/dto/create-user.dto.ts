9bc044caa3c53f899561d30343eb65f8502234b8
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-17 16:15:55 -0500

    🦁 validate incoming user creation requests

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,12 +1,26 @@
 import {
+  IsString,
   IsEmail,
+  MinLength,
+  MaxLength,
+  Matches,
 } from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
+  @MinLength(3, { message: 'Username must be at least 3 characters long' })
+  @MaxLength(20, { message: 'Username must not exceed 20 characters' })
   username: string;
 
   @IsEmail({}, { message: 'Invalid email format' })
   email: string;
 
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(32, { message: 'Password must not exceed 32 characters' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password is too weak. It should contain at least one uppercase letter, one lowercase letter, and one number or special character',
+  })
   password: string;
 }
