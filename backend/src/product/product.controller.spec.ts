5dc9825359870cb54d742a7cba4eddcd4f5af058
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-16 19:39:43 -0500

    📦 pass the product.controller test

--- backend/src/product/product.controller.spec.ts
+++ backend/src/product/product.controller.spec.ts
@@ -1,17 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 describe('ProductController', () => {
   let controller: ProductController;
+  let service: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        { provide: getRepositoryToken(Product), useClass: Repository },
+      ],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
+    service = module.get<ProductService>(ProductService);
   });
 
   it('should be defined', () => {
