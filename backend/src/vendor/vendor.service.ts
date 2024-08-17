ccdc95aabbd8b1257a4958a03fec8ee144a5e6e2
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-17 06:38:43 -0500

    🎊 pass the vendor creation unit test

--- backend/src/vendor/vendor.service.ts
+++ backend/src/vendor/vendor.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { CreateVendorDto } from './dto/create-vendor.dto';
 import { UpdateVendorDto } from './dto/update-vendor.dto';
 import { Vendor } from './entities/vendor.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class VendorService {
-  create(createVendorDto: CreateVendorDto) {
-    return 'This action adds a new vendor';
+  constructor(
+    @InjectRepository(Vendor)
+    private vendorRepository: Repository<Vendor>,
+  ) {}
+
+  async create(createVendorDto: CreateVendorDto): Promise<Vendor> {
+    const vendor = this.vendorRepository.create(createVendorDto);
+    return this.vendorRepository.save(vendor);
   }
 
   findAll() {
