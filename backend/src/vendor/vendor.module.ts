1cb4b0e1fb19bcf7740b648eb54823d143bf0e8f
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-17 16:31:04 -0500

    🫡 Provide the repository to inject it into services

--- backend/src/vendor/vendor.module.ts
+++ backend/src/vendor/vendor.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { VendorService } from './vendor.service';
 import { VendorController } from './vendor.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vendor } from './entities/vendor.entity';
 
 @Module({
   controllers: [VendorController],
   providers: [VendorService],
+  imports: [TypeOrmModule.forFeature([Vendor])],
+  exports: [VendorService],
 })
 export class VendorModule {}
