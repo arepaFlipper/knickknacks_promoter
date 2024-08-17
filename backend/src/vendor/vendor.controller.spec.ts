68731764b9567216aec5c84693263acd47a14865
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-17 06:05:46 -0500

    🏯 fix the vendor.controller default test

--- backend/src/vendor/vendor.controller.spec.ts
+++ backend/src/vendor/vendor.controller.spec.ts
@@ -1,14 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VendorController } from './vendor.controller';
 import { VendorService } from './vendor.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Vendor } from './entities/vendor.entity';
+import { Repository } from 'typeorm';
 
 describe('VendorController', () => {
   let controller: VendorController;
+  let service: VendorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VendorController],
-      providers: [VendorService],
+      providers: [
+        VendorService,
+        { provide: getRepositoryToken(Vendor), useClass: Repository },
+      ],
     }).compile();
 
     controller = module.get<VendorController>(VendorController);
