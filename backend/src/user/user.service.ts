d605deb0eecfdf5447d84a66180ffeb852f78b88
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-17 15:40:31 -0500

    🌝 pass the user creation unit test

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  async create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const exitingUser = await this.userRepository.findOne({
+      where: { username: createUserDto.username },
+    });
+    if (exitingUser) {
+      throw new ConflictException('Username already exists');
+    }
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
   }
 
   async findAll() {
