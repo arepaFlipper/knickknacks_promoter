ac1fc73c56d27a4d0f2c14d915ec6e51f28ea7d4
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-18 13:15:53 -0500

    🌺 add script to delete picture

--- backend/test/picture/delete.py
+++ backend/test/picture/delete.py
@@ -4,18 +4,18 @@ import requests
 BASE_URL = "http://localhost:3000"
 
 # The ID of the picture to update
-PICTURE_ID = 1
+PICTURE_ID = 3
 
 # Define the payload with the changes you want to make
 
 # Send the PATCH request to update the picture
-response = requests.get(f"{BASE_URL}/picture/{PICTURE_ID}")
+response = requests.delete(f"{BASE_URL}/picture/{PICTURE_ID}")
 
 # Check if the request was successful
 if response.status_code == 200:
-    print("Picture retrieved successfully!")
-    print("Picture Data:", response.json())
+    print("Picture deleted successfully!")
+    print("deleted Picture Data:", response)
 else:
-    print(f"Failed to delete picture. Status code: {response.status_code}")
+    print(f"Failed to delete Picture. Status code: {response.status_code}")
     print("Response:", response.json())
 
