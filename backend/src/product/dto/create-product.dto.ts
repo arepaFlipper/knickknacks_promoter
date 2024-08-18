7ac7df9b46110645fa5808c12f65c4a3bd98f74f
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-18 10:00:01 -0500

    🖥️ validate product data before save it

--- backend/src/product/dto/create-product.dto.ts
+++ backend/src/product/dto/create-product.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProductDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsDecimal,
+  IsInt,
+  Min,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @Min(0)
+  price: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  vendorId: number;
+
+  @IsInt()
+  @IsOptional()
+  userId?: number;
+}
