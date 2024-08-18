1aca416e5249bf9655656d3ef965d58c89ffd7f4
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-17 19:32:01 -0500

    👚 add devserver cofigurations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,9 +11,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(databaseConfig),
     ProductModule,
     PictureModule,
