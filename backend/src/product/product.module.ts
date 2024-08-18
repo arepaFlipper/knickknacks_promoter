2a3ae7b5eaeba62f9897fd9a0c087d2be16c46e6
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-18 10:00:30 -0500

    🎖️ register a vendor in a product

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -3,9 +3,11 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { Vendor } from 'src/vendor/entities/vendor.entity';
+import { VendorModule } from 'src/vendor/vendor.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, Vendor]), VendorModule],
   controllers: [ProductController],
   providers: [ProductService],
   exports: [ProductService],
--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,20 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
+import { Vendor } from 'src/vendor/entities/vendor.entity';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product)
     private productRepository: Repository<Product>,
+    @InjectRepository(Vendor)
+    private readonly vendorRepository: Repository<Vendor>,
   ) {}
 
   async create(createProductDto: CreateProductDto): Promise<Product> {
-    const product = this.productRepository.create(createProductDto);
-    return this.productRepository.save(product);
+    try {
+      let vendor: Vendor | undefined;
+      if (createProductDto.vendorId) {
+        vendor = await this.vendorRepository.findOne({
+          where: { id: createProductDto.vendorId },
+        });
+      }
+      const productData = {
+        ...createProductDto,
+        vendor,
+      };
+      const product = this.productRepository.create(productData);
+      return this.productRepository.save(product);
+    } catch (error) {
+      throw new NotFoundException(`The data is invalid`);
+    }
   }
 
   async findAll(): Promise<Product[]> {
