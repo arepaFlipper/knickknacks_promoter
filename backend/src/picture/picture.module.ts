1b71abbb785311cf811b3e12305735365f30a153
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-18 12:22:05 -0500

    💚 add user & product modules into picture module

--- backend/src/picture/picture.module.ts
+++ backend/src/picture/picture.module.ts
@@ -3,9 +3,17 @@ import { PictureService } from './picture.service';
 import { PictureController } from './picture.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Picture } from './entities/picture.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Product } from 'src/product/entities/product.entity';
+import { ProductModule } from 'src/product/product.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Picture])],
+  imports: [
+    TypeOrmModule.forFeature([Picture, User, Product]),
+    ProductModule,
+    UserModule,
+  ],
   controllers: [PictureController],
   providers: [PictureService],
   exports: [PictureService],
